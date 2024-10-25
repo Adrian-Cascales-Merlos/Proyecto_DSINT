7e98e881abdb0eb585199e39356bacb2775d3e50
Samuel Martínez Fernández <samuel.m.f@um.es>
2024-10-25 19:40:25 +0200

    Primeras clases
    Creado los paquetes donde vamos a estructurar el proyecto (en principio).
    
    Creadas las clases de los conceptos de la ontología. Faltan solo las clases Estados y Capacidades (debemos de pensar cómo hacerlas bien).
    
    Revisar los TODOs y las cosas puestas en el documento de la Fase 2

---
+++ work_fase2/DSI_KS_OPC_Las_Cabezas_de_Cerbero/src/main/java/dsi/mitologia/Mortales.java
@@ -0,0 +1,11 @@
+package dsi.mitologia;
+
+import java.util.Set;
+
+public class Mortales extends Seres {
+	
+	public Mortales(String tiene_nombre, Set<Estados> tiene_condicion) {
+		super(tiene_nombre, tiene_condicion);
+	}
+	
+}
---
+++ work_fase2/DSI_KS_OPC_Las_Cabezas_de_Cerbero/src/main/java/dsi/mitologia/Semidioses.java
@@ -0,0 +1,11 @@
+package dsi.mitologia;
+
+import java.util.Set;
+
+public class Semidioses extends Seres {
+	
+	public Semidioses(String tiene_nombre, Set<Estados> tiene_condicion) {
+		super(tiene_nombre, tiene_condicion);
+	}
+
+}
